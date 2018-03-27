4d3e2ef23ce565790bfe58245a311208d59b212f
Monserrat <monserratdiaz12@gmail.com>
2018-03-27 01:00:35 +0000

    basic + random

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 
 $(document).ready(function() {
     function callGiphyAPIWithSearchTerm(searchTerm) {
+    
         var i = Math.floor(Math.random() * (10 + 1));
         $.ajax({
             url: "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC",
@@ -15,38 +16,57 @@ $(document).ready(function() {
                 appendImageToBody(url);
             },
         });
+          
     }
+    
+
     function appendImageToBody(srcURL) {
         $('#results').append(`<img id='gif' src=  ${srcURL}  >`);
         $("h3").hide();
     }
     var searchTerm;
     $("input").keyup(function(event) {
-		if (event.keyCode === 13) {
-			$("button").click();
-		}
-	});
-    $("button").click(function() {
+        if (event.keyCode === 13) {
+            $("#search").click();
+        }
+    });
+    $("#search").click(function() {
         searchTerm = $("input").val();
         callGiphyAPIWithSearchTerm(searchTerm);
         $("input").val("");
     });
 
-    
-    
+
+
     //random images
-    $("#random").click(function(){
-        
+
+    function randomGiphyUrl() {
+
+        var randomUrl = " https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC";
+
+        $.ajax({
+            url: randomUrl,
+            method: "GET",
+            success: function(response) {
+                console.log(response);
+                var url = response.data.images.original.url;
+                appendImageToBody(url);
+            },
+        });
+    }
+    $("#random").click(function() {
+        randomGiphyUrl();
     });
+
 
     // click gif to hide
-    
-    
+
+
     $("#gif").click(function() {
-      $("#gif").hide(); 
-      alert("worked");
+        $("#gif").hide();
+        alert("worked");
     });
 
-    
+
 
 });
